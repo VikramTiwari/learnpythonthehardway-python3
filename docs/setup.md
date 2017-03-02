4158e2f5f571919052932a65e7dd23558750ae60
Vikram Tiwari <vikramtheone1@gmail.com>
2017-03-02 06:58:51 -0800

    setup
    - dox
    - script

---
+++ docs/setup.md
@@ -0,0 +1,10 @@
+# Setup
+
+## Installing python3 on mac
+
+Recommended
+`brew install python3`
+
+
+Easy setup for project
+`source scripts/setup.sh`
---
+++ scripts/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# install brew, if its already installed the script will update it
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# install python3
+brew install python3
