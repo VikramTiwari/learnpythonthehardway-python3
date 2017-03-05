e37e39b4035dbb7cb797980f0d16b2f72cb32f88
Vikram Tiwari <vikramtheone1@gmail.com>
2017-03-04 17:25:43 -0800

    [broken] fix this
    - ex6
    - come back to this to fix it

--- lpthw/ex6.py
+++ lpthw/ex6.py
@@ -1,5 +1,5 @@
 # left out assignment for types_of_people mentioned in intro
-types_of_people = 10
+type_of_people = 10
 # change variable from 10 to types_of_people
 x = f"There are {types_of_people} types of people."
 
@@ -7,21 +7,21 @@ x = f"There are {types_of_people} types of people."
 binary = "binary"
 do_not = "don't"
 # assignment of formatted string to a variable
-y = f"Those who know {binary} and those who {do_not}."
+y = f"Those who know {binary} and those who {don't}."
 
 # print strings
 print(x)
 print(y)
 
 # left out f in front of string and omit extra period
-print(f"I said: {x}")
+print("I said: {x}")
 # left out f in front of string and omit extra period
-print(f"I also said: '{y}'")
+print(f'I also said: '{y}'')
 
 # vaiables initialization
-hilarious = False
+hilarious = false
 # initialization of a string without a placeholder for variable to be input in realtime/runtime
-joke_evaluation = "Isn't that joke so funny?! {}"
+joke_evaluation = "Isn't that joke so funny?!"
 
 # assign string to the variable using .format method
 print(joke_evaluation.format(hilarious))
@@ -31,7 +31,7 @@ w = "This is the left side of..."
 e = "a string with a right side."
 
 # join two strings together
-print(w + e)
+print(e + w)
 # ^ this works because when + operation is performed on strings, it joins them together rathen than trying to perform a mathematical addition operation
 
 # change "What You Should See" snapshot to reflect changes
