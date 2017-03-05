369dbad000c6921a47e46c3775cd33fbca44055c
Vikram Tiwari <vikramtheone1@gmail.com>
2017-03-04 17:23:28 -0800

    study drill ex6

--- lpthw/ex6.py
+++ lpthw/ex6.py
@@ -3,10 +3,13 @@ types_of_people = 10
 # change variable from 10 to types_of_people
 x = f"There are {types_of_people} types of people."
 
+# variables initialization
 binary = "binary"
 do_not = "don't"
+# assignment of formatted string to a variable
 y = f"Those who know {binary} and those who {do_not}."
 
+# print strings
 print(x)
 print(y)
 
@@ -15,15 +18,20 @@ print(f"I said: {x}")
 # left out f in front of string and omit extra period
 print(f"I also said: '{y}'")
 
+# vaiables initialization
 hilarious = False
+# initialization of a string without a placeholder for variable to be input in realtime/runtime
 joke_evaluation = "Isn't that joke so funny?! {}"
 
+# assign string to the variable using .format method
 print(joke_evaluation.format(hilarious))
 
+# vaiables initialization
 w = "This is the left side of..."
 e = "a string with a right side."
 
+# join two strings together
 print(w + e)
-
+# ^ this works because when + operation is performed on strings, it joins them together rathen than trying to perform a mathematical addition operation
 
 # change "What You Should See" snapshot to reflect changes
