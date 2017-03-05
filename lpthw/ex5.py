c31384ff393f55908b101c8e33a47c268e828277
Vikram Tiwari <vikramtheone1@gmail.com>
2017-03-04 16:58:35 -0800

    study drill ex5

--- lpthw/ex5.py
+++ lpthw/ex5.py
@@ -1,18 +1,24 @@
-my_name = 'Zed A. Shaw'
-my_age = 35 # not a lie
-my_height = 74 # inches
-my_weight = 180 # lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
 
-print(f"Let's talk about {my_name}.")
-print(f"He's {my_height} inches tall.")
-print(f"He's {my_weight} pounds heavy.")
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {weight} pounds heavy.")
 print("Actually that's not too heavy.")
-print(f"He's got {my_eyes} eyes and {my_hair} hair.")
-print(f"His teeth are usually {my_teeth} depending on the coffee.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
 
 # this line is tricky, try to get it exactly right
-total = my_age + my_height + my_weight
-print(f"If I add {my_age}, {my_height}, and {my_weight} I got {total}.")
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I got {total}.")
+
+# converting values in metric system
+metric_height = height * 0.0254 # 1 inch = 0.0254 meter
+metric_weight = weight * 0.453592 # 1 pound = 0.453592 kilogram
+print(f"My height is {metric_height} meters.")
+print(f"My weight is {metric_weight} kilograms.")
