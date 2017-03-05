583df8b94d567229e1286e1a5044b264e4b0402a
Vikram Tiwari <vikramtheone1@gmail.com>
2017-03-04 19:39:16 -0800

    ex10 study drill

--- lpthw/ex10.py
+++ lpthw/ex10.py
@@ -2,14 +2,25 @@ tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
 
-fat_cat = """
+fat_cat = '''
 I'll do a list:
 \t* Cat food
 \t* Fishies
 \t* Catnip\n\t* Grass
-"""
+'''
 
 print(tabby_cat)
 print(persian_cat)
 print(backslash_cat)
 print(fat_cat)
+
+# mix some things up
+formatter = '{} {} {}'
+
+hi = '''
+How you doing?
+\v\a'''
+hello = '\tHola! How\'s \"it\" going?'
+bye = "\nGotta go! Bye."
+
+print(formatter.format(hi, hello, bye))
