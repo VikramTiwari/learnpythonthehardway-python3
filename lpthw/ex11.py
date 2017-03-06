ccdc5830a9f445ca9e3e3aae0f598cae3b405191
Vikram Tiwari <vikramtheone1@gmail.com>
2017-03-05 23:33:52 -0800

    study drills ex11

--- lpthw/ex11.py
+++ lpthw/ex11.py
@@ -7,3 +7,10 @@ weight = input()
 
 
 print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+print("What is your name?", end=' ')
+name = input()
+print("What is your superhero name?", end=' ')
+superhero_name = input()
+
+print(f"Hello {name}, or should I call you {superhero_name}?")
